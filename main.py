22f01568c9f860f6787661bcfa3f9e7cdabfa92f
Aasthuu <164446575+Aasthuu@users.noreply.github.com>
2024-07-31 21:35:08 -0400

    new update

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    random_number = random.randint(1, 100)
+    return {"Hello": random_number}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
